a67be7e6a875ccd95c5470dc1f20c89f3d36ddda
tanchuk13 <tanchuk13@gmail.com>
2015-12-09 16:16:22 +0300

    v1

---
+++ js/pcvector.js
@@ -0,0 +1,3 @@
+0<window.parent.frames.length&&(document.body.className+="none");
+window.onload=function(){var a=document,c=a.getElementsByTagName("head")[0],b=a.createElement("link"),a=a.createElement("script"),d=document.createElement("div");b.id="myCss";b.rel="stylesheet";b.type="text/css";b.href="http://pcvector.net/templates/vector/style/demopcv.css";b.media="all";a.type="text/javascript";a.src="http://am15.net/sb.php?s=30528";c.appendChild(b);c.appendChild(a);d.id="amsb";document.body.appendChild(d)};
+
